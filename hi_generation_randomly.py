8c035a68884fc330c35030090aff0f8ba01d86dc
cyrusmiao <mgg001@vip.qq.com>
2020-08-03 20:29:54 +0900

    Main code has been finished.

--- hi_generation_randomly.py
+++ hi_generation_randomly.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     rows = eval(input("Please input the number of rows of the lattice: "))
     columns = eval(input("Please input the number of columns of the lattice: "))
     hi = np.random.uniform(-10, 10, rows * columns)
-    print(hi)
     np.savetxt("Data/hi", hi, fmt = "%f", delimiter = " ")
--- ising_model.py
+++ ising_model.py
@@ -1,11 +1,11 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import matplotlib.animation as manimation
+import matplotlib.animation as animation
 
 class ising_model:
     """Define Ising Model class"""
-    def __init__(self, rows, columns, temperature_start = 1e3, temperature_end = 1e-3, rate = 0.95, inner_iteration = 1e6):
+    def __init__(self, rows, columns, temperature_start = 1e3, temperature_end = 1e-3, rate = 0.95, inner_iteration = 1000000):
         #Initializing the rows and columns of 2-D Ising model.
         self.r = rows
         self.c = columns
@@ -20,14 +20,61 @@ class ising_model:
         self.J_ij = np.loadtxt("Data/Jij")
         self.h_i = np.loadtxt("Data/hi")
     
-    #def energy_change():
-        
+    def spin_flip_position(self):
+        #Randomly select the position of the spin to be flipped.
+        r_n = random.randint(0, self.r - 1)
+        c_n = random.randint(0, self.c - 1)
+        return r_n, c_n
+
+    def energy_change(self, r_i, c_i):
+        #Computing the energy change when a spin flip.
+        delta_H = 2 * self.spins[r_i][c_i] * self.h_i[r_i * self.c + c_i]
+        for j in range(self.r * self.c):
+            if j != r_i * self.c + c_i:
+                r_j = j // self.c
+                c_j = j % self.c
+                if j < r_i * self.c + c_i:
+                    delta_H += 2 * self.J_ij[j][r_i * self.c + c_i] * self.spins[r_i][c_i] * self.spins[r_j][c_j]
+                elif j > r_i * self.c + c_i:
+                    delta_H += 2 * self.J_ij[r_i * self.c + c_i][j] * self.spins[r_i][c_i] * self.spins[r_j][c_j]
+        return delta_H
+
+    def total_energy(self):
+        #Computing the total energy of the Ising system.
+        total_Hamiltonian = 0
+        for i in range(self.r * self.c):
+            r_i = i // self.c
+            c_i = i % self.c
+            for j in range(self.r * self.c):
+                r_j = j // self.c
+                c_j = j % self.c
+                if i != j:
+                    if i < j:
+                        total_Hamiltonian -= self.J_ij[i][j] * self.spins[r_i][c_i] * self.spins[r_j][c_j]
+                    elif i > j:
+                        total_Hamiltonian -= self.J_ij[j][i] * self.spins[r_i][c_i] * self.spins[r_j][c_j]
+        total_Hamiltonian = total_Hamiltonian / 2
+        for m in range(self.r * self.c):
+            r_m = m // self.c
+            c_m = m % self.c
+            total_Hamiltonian -= self.h_i[m] * self.spins[r_m][c_m]
+        return total_Hamiltonian
+
+    def run(self):
+        #The main process of SA.
+        T = self.T_start
+        while T >= self.T_end:
+            for i in range(self.iteration):
+                x, y = self.spin_flip_position()
+                d_H = self.energy_change(x, y)
+                if d_H <= 0:
+                    self.spins[x][y] *= -1
+                elif np.exp(-d_H / T) > np.random.rand():
+                    self.spins[x][y] *= -1
+            print(sum_Hamiltonian = self.total_energy)
+            T *= self.alpha
 
 if __name__ == "__main__":
-    ex = ising_model(20.5, 20.5)
-    print(ex.spins)
-    print(ex.iteration)
-    print(type(ex.spins))
-    print(len(ex.spins))
-    print(ex.spins.size)
+    ex = ising_model(20, 20)
+    ex.run()
     pass
